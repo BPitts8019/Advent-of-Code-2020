ca283f0a74e5c99487a969c05b4bfaf4c6e012ff
BPitts8019 <bradleyj.pitts@gmail.com>
2020-12-15 10:37:06 -0700

    chore: setup day01b tests

--- __tests__/day-01.test.js
+++ __tests__/day-01.test.js
@@ -1,5 +1,7 @@
-const multiplyEntries = require("../src/day-01/expenseReport").multiplyEntries;
-// const partTwo = require("../src/day-01/expenseReport").multiplyEntries;
+const {
+   multiplyEntries,
+   multiplyExtraEntries,
+} = require("../src/day-01/expenseReport");
 
 /*
 Part 1 Test Requirements
@@ -7,8 +9,8 @@ Find two entries in the expense report that sums to 2020, and return thier produ
 */
 describe("Day 01a: Expense Report - Two Entries", () => {
    it("Can handle a list with unique values", () => {
-      const input = [1234, 220, 354, 4768, 598, 632, 786];
-      expect(multiplyEntries(input)).toBe(1234 * 786);
+      const input = [1721, 979, 366, 299, 675, 1456];
+      expect(multiplyEntries(input)).toBe(1721 * 299);
    });
    it("Can handle a list with duplicate values", () => {
       const input = [252, 1768, 1768, 1768, 598, 632, 786];
@@ -27,18 +29,14 @@ describe("Day 01a: Expense Report - Two Entries", () => {
 /*
 Part 2 Test Requirements
 */
-describe("Day 01a: Expense Report - Three Entries", () => {
+describe("Day 01b: Expense Report - Three Entries", () => {
    it("Can handle a list with unique values", () => {
-      const input = [1234, 220, 354, 4768, 598, 632, 786];
-      expect(multiplyExtraEntries(input)).toBe(1234 * 786);
+      const input = [1721, 979, 366, 299, 675, 1456];
+      expect(multiplyExtraEntries(input)).toBe(979 * 366 * 675);
    });
    it("Can handle a list with duplicate values", () => {
-      const input = [252, 1768, 1768, 1768, 598, 632, 786];
-      expect(multiplyExtraEntries(input)).toBe(252 * 1768);
-   });
-   it("Can find a result where the two numbers are equal", () => {
-      const input = [1768, 1010, 1768, 598, 632, 1010];
-      expect(multiplyExtraEntries(input)).toBe(1010 * 1010);
+      const input = [252, 1768, 884, 884, 598, 632, 786];
+      expect(multiplyExtraEntries(input)).toBe(252 * 884 * 884);
    });
    it("Can fail to find a result", () => {
       const input = [1, 2, 3, 4, 5, 6];
